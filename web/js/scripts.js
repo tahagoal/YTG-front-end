e81ba2064279fe5413cbbaa8eebe157e65f73bcd
ahmed taha <ahmed@Ahmed.kuikdelivery.com>
2018-11-03 18:49:42 +0200

    home draft

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -30,31 +30,31 @@ $(document).ready(function($) {
 			if (st > 150) {
 				if ( !navbar.hasClass('scrolled') ) {
 					navbar.addClass('scrolled');	
-				}
-			} 
-			if (st < 150) {
-				if ( navbar.hasClass('scrolled') ) {
-					navbar.removeClass('scrolled sleep');
-				}
-			} 
-			if ( st > 350 ) {
-				if ( !navbar.hasClass('awake') ) {
+                }
+                
+                if ( !navbar.hasClass('awake') ) {
 					navbar.addClass('awake');	
 				}
 				
 				if(sd.length > 0) {
 					sd.addClass('sleep');
-				}
-			}
-			if ( st < 350 ) {
-				if ( navbar.hasClass('awake') ) {
+                }
+                
+			} 
+			if (st < 150) {
+				if ( navbar.hasClass('scrolled') ) {
+					navbar.removeClass('scrolled sleep');
+                }
+                
+                if ( navbar.hasClass('awake') ) {
 					navbar.removeClass('awake');
 					navbar.addClass('sleep');
 				}
 				if(sd.length > 0) {
 					sd.removeClass('sleep');
 				}
-			}
+            } 
+            
 		});
 	};
 	scrollWindow();
