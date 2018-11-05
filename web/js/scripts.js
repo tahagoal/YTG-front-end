1515cb689eab14d1ec40757951ab083727a6add2
ahmed taha <ahmed@Ahmed.kuikdelivery.com>
2018-11-05 23:04:15 +0200

    home page 1st deaft

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -45,7 +45,7 @@ $(document).ready(function($) {
 				if ( navbar.hasClass('scrolled') ) {
 					navbar.removeClass('scrolled sleep');
                 }
-                
+
                 if ( navbar.hasClass('awake') ) {
 					navbar.removeClass('awake');
 					navbar.addClass('sleep');
@@ -190,3 +190,19 @@ $(document).ready(function($) {
 	OnePageNav();
 
 });
+
+
+// Function to reveal lightbox and adding YouTube autoplay
+function revealVideo(div,video_id) {
+  var video = document.getElementById(video_id).src;
+  document.getElementById(video_id).src = video+'&autoplay=1'; // adding autoplay to the URL
+  document.getElementById(div).style.display = 'block';
+}
+
+// Hiding the lightbox and removing YouTube autoplay
+function hideVideo(div,video_id) {
+  var video = document.getElementById(video_id).src;
+  var cleaned = video.replace('&autoplay=1',''); // removing autoplay form url
+  document.getElementById(video_id).src = cleaned;
+  document.getElementById(div).style.display = 'none';
+}
