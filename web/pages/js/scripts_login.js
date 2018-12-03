34aa071dd4113ca32688a4bb0b6429460686869c
ahmed taha <ahmed@Ahmed.kuikdelivery.com>
2018-12-03 23:23:35 +0200

    asd

--- web/pages/js/scripts_login.js
+++ web/pages/js/scripts_login.js
@@ -45,6 +45,27 @@ $(document).ready(function ($) {
 
 
 
+	gapi.load('auth2', function(){
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      auth2 = gapi.auth2.init({
+        client_id: '364459077088-8kght2lqtpd8niqb1co1duq3nmrpnjt4.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        // Request scopes in addition to 'profile' and 'email'
+        //scope: 'additional_scope'
+      });
+      attachSignin(document.getElementById('customBtn'));
+    });
+
+    function attachSignin(element) {
+	    console.log(element.id);
+	    auth2.attachClickHandler(element, {},
+	        function(googleUser) {
+		         onSignIn(googleUser);
+	        }, function(error) {
+	          alert(JSON.stringify(error, undefined, 2));
+	        });
+	  }
+
 	window.fbAsyncInit = function () {
 		FB.init({
 			appId: '470191943386802',
