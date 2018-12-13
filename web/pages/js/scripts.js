51f05d43a1605b18b0f758d010540daa460a8e92
Ahmed Taha <ahmed.taha@robovics.com>
2018-12-13 16:39:08 +0200

    asd

--- web/pages/js/scripts.js
+++ web/pages/js/scripts.js
@@ -271,6 +271,10 @@ $(document).ready(function ($) {
 		// localStorage.clear();
 		localStorage.removeItem('token');
 		localStorage.removeItem('user_id');
+		var auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+            console.log("on sign out");
+        });
 	    document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); })
 	    setTimeout(function(){
 		    window.location.href = 'home.html';
