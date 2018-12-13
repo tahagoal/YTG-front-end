34a0e55f907caf6dea9e8afab29be369be7a4c48
ahmed taha <tahagoal@gmail.com>
2018-12-13 23:03:21 +0200

    asd

--- web/pages/js/scripts_login.js
+++ web/pages/js/scripts_login.js
@@ -61,10 +61,13 @@ $(document).ready(function ($) {
 		middle_auth();
 	}
 	else{
-		var auth2 = gapi.auth2.getAuthInstance();
-	    auth2.signOut().then(function () {
-	      // console.log('User signed out.');
-	    });
+		setTimeout(function(){
+			var auth2 = gapi.auth2.getAuthInstance();
+		    auth2.signOut().then(function () {
+		      // console.log('User signed out.');
+		    });
+		,100})
+		
 	}
 
 	check_ath();
